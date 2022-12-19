ff1114ece57c68d856fbd5676196fcc0f413b8bf
DouglasOliveiraLeite <devdouglasleite@gmail.com>
2022-12-19 19:48:30 -0300

    adc botoes, cores aleatórias e resolvido ate o requisito 4

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const getPaleta = document.getElementById('Paleta');
+const getButton = document.getElementById('button-random-color');
 
 const createHeader = () => {
   const createHeaders = document.createElement('header');
@@ -19,6 +20,10 @@ const generatePalette = () => {
     const criaPalette = document.getElementById('color-palette');
     const criaDiv = document.createElement('div');
     criaPalette.appendChild(criaDiv);
+    criaDiv.className = 'color';
+    criaDiv.style.border = 'solid 1px black';
+    criaDiv.style.width = '30px';
+    criaDiv.style.height = '30px';
   }
 };
 
@@ -26,7 +31,31 @@ function createPalette() {
   const createDiv = document.createElement('div');
   createDiv.id = 'color-palette';
   getPaleta.appendChild(createDiv);
+  createDiv.style.display = 'flex';
+  createDiv.style.justifyContent = 'center';
+  createDiv.style.gap = '18px';
 }
+
+const randomColor = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+const pintarPaleta = () => {
+  const getColor = document.querySelectorAll('.color');
+  for (let i = 0; i < getColor.length; i += 1) {
+    if (i === 0) {
+      getColor[i].style.backgroundColor = 'black';
+    } else {
+      getColor[i].style.backgroundColor = randomColor();
+    }
+  }
+};
+
+getButton.addEventListener('click', pintarPaleta);
 
 // execut functions //
 window.onload = () => {
@@ -34,4 +63,5 @@ window.onload = () => {
   createH1();
   createPalette();
   generatePalette();
+  pintarPaleta();
 };
